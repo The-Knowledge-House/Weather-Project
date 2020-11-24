0e905fc976359626f032ef64c2ce98ce72e83a08
Shafee <shafeelinks@gmail.com>
2020-11-24 15:29:56 -0500

    added icons and description for weather output

--- main.js
+++ main.js
@@ -6,21 +6,26 @@ dotenv.config();
 const BASE_URL = process.env.BASE_URL;
 const API_KEY = process.env.API_KEY;
 
-/*
 
-axios.get(`${BASE_URL}/data/2.5/weather?q={city name}&appid={API_KEY}`)
 
-*/
-const body = document.querySelector('body');
 const input = document.querySelector('#city-search');
 const searchBtn = document.querySelector('#search');
 const cityName = document.querySelector('#city-name');
 const temperature = document.querySelector('#temperature');
+const description = document.querySelector('#desc');
+const icon = document.querySelector('#icon');
+const darkDiv = document.querySelector('.dark');
 
+window.addEventListener('load', (event) => {
+    document.querySelector('html').classList.add('trans');
+});
 
+darkDiv.classList.remove("dark");
 searchBtn.addEventListener('click', async (url) => {
+    darkDiv.classList.add('dark');
     try {
         if (input.value) {
+
             let tempUnit = `\u00B0 F`;
             // getting weather data
             url = await axios.get(`${BASE_URL}/data/2.5/weather?q=${input.value}&appid=${API_KEY}`);
@@ -28,6 +33,45 @@ searchBtn.addEventListener('click', async (url) => {
             // display temperature
             cityName.innerText = `${url.data.name}, ${url.data.sys.country}`;
             temperature.innerText = `${kelvinToFahrenheit(url.data.main.temp)} ${tempUnit}`;
+            let weatherMain = url.data.weather[0].main;
+            let weatherDescription = url.data.weather[0].description;
+            description.innerText = weatherDescription;
+            const hr = (new Date()).getHours();
+            switch (weatherMain) {
+                case 'Thunderstorm':
+                    icon.classList.add('wi', 'wi-day-lightning');
+                    break;
+                case 'Drizzle':
+                    icon.classList.add('wi', 'wi-showers');
+                    break;
+                case 'Rain':
+                    icon.classList.add('wi', 'wi-rain');
+                    break;
+                case 'Snow':
+                    icon.classList.add('wi', 'wi-snow-wind');
+                    break;
+                case ('Mist' || 'Haze' || 'Fog' || 'Smoke'):
+                    icon.classList.add('wi', 'wi-fog');
+                    break;
+                case ('Sand' || 'Dust'):
+                    icon.classList.add('wi', 'wi-sandstorm');
+                    break;
+                case 'Tornado':
+                    icon.classList.add('wi', 'wi-tornado');
+                    break;
+                case 'Clear':
+                    hr > 6 < 18 ?
+                        icon.classList.add('wi', 'wi-horizon-alt') :
+                        icon.classList.add('wi', 'wi-night-clear');
+                    break;
+                case 'Clouds':
+                    icon.classList.add('wi', 'wi-cloudy');
+                    break;
+                default:
+                    icon.classList.add('wi', 'wi-na');
+                    break;
+            }
+
             temperature.addEventListener('click', () => {
                 if (tempUnit === `\u00B0 F`) {
                     tempUnit = `\u00B0 C`;
