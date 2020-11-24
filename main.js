24b6265ed1c677da996b46fc8462e6c21163f157
david espinal <despinal0425@gmail.com>
2020-11-24 08:11:56 -0500

    Working on getting data from weather API

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const getWeather = document.getElementById("getTemp");
+const searchWeatherInput = document.getElementById("city");
+
+
+
 //remember, make a .env file and save your key
-const api_key = 'Your key here';
+getWeather.addEventListener('click', async (e) => { 
+  e.preventDefault(); //this line helps make the input act like a button 
+  let cityWeather = searchWeatherInput.value;
+  const weatherUrl = `${process.env.API_BASE_URL}/data/2.5/weather?q=${cityWeather}&units=imperial&appid=${process.env.API_KEY}`;
+  
+  try {
+    let response = await axios.get(weatherUrl)
+    console.log(response);
+  } catch { 
+    console.log(e);
+  }
+})
