b1b0b68029e7c2d02dcb9cb6607e12d0e3b1e6e7
katfernandez22 <60719712+katfernandez22@users.noreply.github.com>
2020-11-29 12:49:02 -0500

    Weather-Project Done

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
-const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+// const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
 //remember, make a .env file and save your key
-const api_key = 'Your key here';
+// const api_key = 'Your key here';
+
+
+import axios from "axios"
+import dotenv from "dotenv"
+dotenv.config();
+const BASE_URL = process.env.BASE_URL
+const API_KEY = process.env.API_KEY
+const button = document.getElementById("getTemp");
+const cityInput = document.getElementById("city");
+
+function weather(cityName) {
+  const url = `${BASE_URL}/data/2.5/weather?q=${cityName}&units=imperial&appid=${API_KEY}`;
+
+  axios.get(url)
+    .then(function (response) {
+      let div = document.createElement('div');
+      let weather1 = document.querySelector('#result-api');
+      weather1.innerHTML = '';
+      div.innerHTML = '<p>' + 'City: ' + '<span>' + data.name + '</span>' + '</p>' +
+        '<p>' + 'Current Temperature: ' + '<span>' + data.main.temp + '&#8457;' + '</span>' + '</p>' +
+        '<p>' + 'Maximum Temperature: ' + '<span>' + data.main.temp_max + '&#8457;' + '</span>' + '</p>' +
+        '<p>' + 'Minimum Temperature: ' + '<span>' + data.main.temp_min + '&#8457;' + '</span>' + '</p>' +
+        '<p>' + 'Weather Description: ' + '<span>' + data.weather[0].descr
+
+
+      console.log(response);
+    })
+    .catch(err => document.getElementById('result-api').innerHTML = "error!")
+
+})
+   
+}
+
