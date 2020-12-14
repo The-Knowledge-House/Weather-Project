de9684eb2ccaf9b037f606f840c05f335cdbef46
katfernandez22 <60719712+katfernandez22@users.noreply.github.com>
2020-12-13 22:17:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,34 +3,47 @@
 // const api_key = 'Your key here';
 
 
-import axios from "axios"
-import dotenv from "dotenv"
-dotenv.config();
+//dotenv.config();
 const BASE_URL = process.env.BASE_URL
 const API_KEY = process.env.API_KEY
-const button = document.getElementById("getTemp");
-const cityInput = document.getElementById("city");
+const button = document.getElementById("submit");
+
 
 function weather(cityName) {
-  const url = `${BASE_URL}/data/2.5/weather?q=${cityName}&units=imperial&appid=${API_KEY}`;
 
-  axios.get(url)
-    .then(function (response) {
-      let div = document.createElement('div');
-      let weather1 = document.querySelector('#result-api');
-      weather1.innerHTML = '';
-      div.innerHTML = '<p>' + 'City: ' + '<span>' + data.name + '</span>' + '</p>' +
-        '<p>' + 'Current Temperature: ' + '<span>' + data.main.temp + '&#8457;' + '</span>' + '</p>' +
-        '<p>' + 'Maximum Temperature: ' + '<span>' + data.main.temp_max + '&#8457;' + '</span>' + '</p>' +
-        '<p>' + 'Minimum Temperature: ' + '<span>' + data.main.temp_min + '&#8457;' + '</span>' + '</p>' +
-        '<p>' + 'Weather Description: ' + '<span>' + data.weather[0].descr
+  $(document).ready(function () {
+    $.ajax({
+      method: "GET",
+      crossDomain: true,
+      dataType: 'json',
+      ContentType: "application/json",
+      url: BASE_URL + "/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + API_KEY,
 
 
-      console.log(response);
+      success: function (response) {
+        let weather1 = document.querySelector('#result-api');
+        const data = response;
+        console.log(data);
+        console.log(weather1);
+        weather1.innerHTML = '<p>' + 'City: ' + '<span>' + data.name + '</span>' + '</p>' +
+          '<p>' + 'Current Temperature: ' + '<span>' + data.main.temp + '&#8457;' + '</span>' + '</p>' +
+          '<p>' + 'Maximum Temperature: ' + '<span>' + data.main.temp_max + '&#8457;' + '</span>' + '</p>' +
+          '<p>' + 'Minimum Temperature: ' + '<span>' + data.main.temp_min + '&#8457;' + '</span>' + '</p>' +
+          '<p>' + 'Weather Description: ' + '<span>' + data.weather[0].description
+      }
+      ,
+      error: function (jqXHR, textStatus, errorThrown) {
+        document.getElementById('result-api').innerHTML = "error!"
+      }
     })
-    .catch(err => document.getElementById('result-api').innerHTML = "error!")
 
-})
-   
+  });
 }
 
+var submit = document.getElementById("submit");
+submit.onclick = function () {
+  const cityInput = document.getElementById("cityInput");
+  console.log(cityInput.value)
+  weather(cityInput.value);
+
+}
